924f4cabc19095ccdf0e382cf2eca8fcbd178b52
vanessa <vanessa.borst@uni-wuerzburg.de>
2024-02-06 15:09:16 +0100

    Refactored summarize_cross_valid_for_paper.py)

--- summarize_cross_valid_for_paper.py
+++ summarize_cross_valid_for_paper.py
@@ -10,15 +10,33 @@ model_paths = {'Baseline': 'BaselineModel_CV/0116_145000_ml_bs64_250Hz_60s',
                'Multibranch': 'Multibranch_MACRO_CV/0201_104057_ml_bs64convRedBlock_333_0.2_6_false_0.2_24'}
 include_acc = False
 
+# The box extends from the Q1 to Q3 quartile values of the data, with a line at the median (Q2).
+# Q1 = 25% percentile, Q2 = 50% percentile, Q3 = 75% percentile
+# The whiskers extend from the edges of box to show the range of the data.
+# The position of the whiskers is set by default to 1.5*IQR (IQR = Q3 - Q1) from the edges of the box.
+# Outlier points are those past the end of the whiskers.
+def create_and_save_boxplot_for_single_model(df, model_name, metric_name, metric_full_name):
+    # Boxplot for Acc
+    x_labels = [w.replace(f'W-AVG_{metric_name}', 'w-AVG').replace(f'm-AVG_{metric_name}', 'm-AVG')
+                for w in df.columns[1:].values.tolist()]
+    ax = df.drop(columns=["Fold"]).boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
+    ax.set_title(f'{model_name} - {metric_name}')
+    ax.set_xlabel('Class name')
+    ax.set_ylabel(metric_full_name)
+    ax.set_xticklabels(x_labels, rotation=30)
+    plt.tight_layout()
+    plt.savefig(os.path.join('figures', f'{model_name} - {metric_name}.pdf'), format="pdf", bbox_inches="tight")
+    plt.show()
 
-def create_and_save_boxplot(df, model_names, metric_name, metric_cols, title, x_label, y_label, filename):
+def create_and_save_boxplot_for_macro_avgs(df, model_names, metric_name, metric_cols, title, x_label, y_label,
+                                           filename):
     x_labels = [
         w.replace(f'_{metric_name}', '').replace('Final_Model', 'MACRO').replace('Multibranch', 'Multibranch \n MACRO')
         for w in df.columns[metric_cols].values.tolist()]
 
     ax = df[[f'{model_names[0]}_{metric_name}',
              f'{model_names[1]}_{metric_name}',
-             f'{model_names[2]}_{metric_name}1']].boxplot(meanline=True, showmeans=True)
+             f'{model_names[2]}_{metric_name}']].boxplot(meanline=True, showmeans=True)
     ax.set_title(title)
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
@@ -41,7 +59,7 @@ df_result_columns = \
 
 df_results_paper = pd.DataFrame(columns=df_result_columns)
 
-df_results_paper['Type'] = ['SNR', 'AF', 'IAVB', 'LBBB', 'RBBB', 'PAC', 'VEB', 'STD', 'STE', 'm-AVG_F1', 'W-AVG_F1']
+df_results_paper['Type'] = ['SNR', 'AF', 'IAVB', 'LBBB', 'RBBB', 'PAC', 'VEB', 'STD', 'STE', 'm-AVG', 'W-AVG']
 df_results_macro_averages = pd.DataFrame(columns=[f'{model_names[0]}_F1',
                                                   f'{model_names[1]}_F1',
                                                   f'{model_names[2]}_F1',
@@ -104,48 +122,15 @@ for model_name, model_path in model_paths.items():
         df_results_acc.loc[len(df_results_acc)] = row_values_acc
 
     # Visualize the results
-
-    # The box extends from the Q1 to Q3 quartile values of the data, with a line at the median (Q2).
-    # Q1 = 25% percentile, Q2 = 50% percentile, Q3 = 75% percentile
-    # The whiskers extend from the edges of box to show the range of the data.
-    # The position of the whiskers is set by default to 1.5*IQR (IQR = Q3 - Q1) from the edges of the box.
-    # Outlier points are those past the end of the whiskers.
-
     # Boxplot for F1
-    x_labels = [w.replace('W-AVG_F1', 'w-AVG').replace('m-AVG_F1', 'm-AVG')
-                for w in df_results_F1.columns[1:].values.tolist()]
-    ax1 = df_results_F1.drop(columns=["Fold"]).boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
-    ax1.set_title(f'{model_name} - F1')
-    ax1.set_xlabel('Class name')
-    ax1.set_ylabel('F1 score')
-    ax1.set_xticklabels(x_labels, rotation=30)
-    plt.tight_layout()
-    plt.savefig(os.path.join('figures', f'{model_name} - F1.pdf'), format="pdf", bbox_inches="tight")
-    plt.show()
-
+    create_and_save_boxplot_for_single_model(df=df_results_F1, model_name=model_name,
+                                             metric_name='F1', metric_full_name='F1 score')
     # Boxplot for AUC
-    x_labels = [w.replace('W-AVG_AUC', 'w-AVG').replace('m-AVG_AUC', 'm-AVG')
-                for w in df_results_AUC.columns[1:].values.tolist()]
-    ax2 = df_results_AUC.drop(columns=["Fold"]).boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
-    ax2.set_title(f'{model_name} - AUC')
-    ax2.set_xlabel('Class name')
-    ax2.set_ylabel('AUC score')
-    ax2.set_xticklabels(x_labels, rotation=30)
-    plt.tight_layout()
-    plt.savefig(os.path.join('figures', f'{model_name} - AUC.pdf'), format="pdf", bbox_inches="tight")
-    plt.show()
-
+    create_and_save_boxplot_for_single_model(df=df_results_AUC, model_name=model_name,
+                                             metric_name='AUC', metric_full_name='AUC score')
     # Boxplot for Acc
-    x_labels = [w.replace('W-AVG_Acc', 'w-AVG').replace('m-AVG_Acc', 'm-AVG')
-                for w in df_results_acc.columns[1:].values.tolist()]
-    ax3 = df_results_acc.drop(columns=["Fold"]).boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
-    ax3.set_title(f'{model_name} - Acc')
-    ax3.set_xlabel('Class name')
-    ax3.set_ylabel('Accuracy')
-    ax3.set_xticklabels(x_labels, rotation=30)
-    plt.tight_layout()
-    plt.savefig(os.path.join('figures', f'{model_name} - Acc.pdf'), format="pdf", bbox_inches="tight")
-    plt.show()
+    create_and_save_boxplot_for_single_model(df=df_results_acc, model_name=model_name,
+                                             metric_name='Acc', metric_full_name='Accuracy')
 
     # Put the macro averages of the current model into the dataframe
     df_results_macro_averages[f'{model_name}_F1'] = df_results_F1['m-AVG_F1']
@@ -153,11 +138,10 @@ for model_name, model_path in model_paths.items():
     df_results_macro_averages[f'{model_name}_Acc'] = df_results_acc['m-AVG_Acc']
 
     # Get statistics
-    df_F1_statistics = df_results_F1.describe().drop(columns=["Fold"])
+    # df_F1_statistics = df_results_F1.describe().drop(columns=["Fold"])
 
     # Append another row containing the mean and std
     df_results_F1 = pd.concat([df_results_F1, df_results_F1.describe().loc[["mean", "std"]]])
-    # df_results_F1.loc['mean'] = df_results_F1.mean()
     df_results_F1 = df_results_F1.round(3)
     df_results_AUC = pd.concat([df_results_AUC, df_results_AUC.describe().loc[["mean", "std"]]])
     df_results_AUC = df_results_AUC.round(3)
@@ -165,41 +149,18 @@ for model_name, model_path in model_paths.items():
     df_results_acc = df_results_acc.round(3)
 
     # Add the average metric_cols across all folds to the final result dataframes
-    df_results_paper[f'{model_name}_F1'] = [df_results_F1.loc['mean']['SNR'],
-                                            df_results_F1.loc['mean']['AF'],
-                                            df_results_F1.loc['mean']['IAVB'],
-                                            df_results_F1.loc['mean']['LBBB'],
-                                            df_results_F1.loc['mean']['RBBB'],
-                                            df_results_F1.loc['mean']['PAC'],
-                                            df_results_F1.loc['mean']['VEB'],
-                                            df_results_F1.loc['mean']['STD'],
-                                            df_results_F1.loc['mean']['STE'],
-                                            df_results_F1.loc['mean']['m-AVG_F1'],
-                                            df_results_F1.loc['mean']['W-AVG_F1']]
+    df_results_paper[f'{model_name}_F1'] = [df_results_F1.loc['mean'][col] for col in
+                                            ['SNR', 'AF', 'IAVB', 'LBBB', 'RBBB', 'PAC', 'VEB', 'STD', 'STE',
+                                             'm-AVG_F1', 'W-AVG_F1']]
 
-    df_results_paper[f'{model_name}_AUC'] = [df_results_AUC.loc['mean']['SNR'],
-                                             df_results_AUC.loc['mean']['AF'],
-                                             df_results_AUC.loc['mean']['IAVB'],
-                                             df_results_AUC.loc['mean']['LBBB'],
-                                             df_results_AUC.loc['mean']['RBBB'],
-                                             df_results_AUC.loc['mean']['PAC'],
-                                             df_results_AUC.loc['mean']['VEB'],
-                                             df_results_AUC.loc['mean']['STD'],
-                                             df_results_AUC.loc['mean']['STE'],
-                                             df_results_AUC.loc['mean']['m-AVG_AUC'],
-                                             df_results_AUC.loc['mean']['W-AVG_AUC']]
+    df_results_paper[f'{model_name}_AUC'] = [df_results_AUC.loc['mean'][col] for col in
+                                             ['SNR', 'AF', 'IAVB', 'LBBB', 'RBBB', 'PAC', 'VEB', 'STD', 'STE',
+                                              'm-AVG_AUC', 'W-AVG_AUC']]
+
     if include_acc:
-        df_results_paper[f'{model_name}_Acc'] = [df_results_acc.loc['mean']['SNR'],
-                                                 df_results_acc.loc['mean']['AF'],
-                                                 df_results_acc.loc['mean']['IAVB'],
-                                                 df_results_acc.loc['mean']['LBBB'],
-                                                 df_results_acc.loc['mean']['RBBB'],
-                                                 df_results_acc.loc['mean']['PAC'],
-                                                 df_results_acc.loc['mean']['VEB'],
-                                                 df_results_acc.loc['mean']['STD'],
-                                                 df_results_acc.loc['mean']['STE'],
-                                                 df_results_acc.loc['mean']['m-AVG_Acc'],
-                                                 df_results_acc.loc['mean']['W-AVG_Acc']]
+        df_results_paper[f'{model_name}_Acc'] = [df_results_acc.loc['mean'][col] for col in
+                                                 ['SNR', 'AF', 'IAVB', 'LBBB', 'RBBB', 'PAC', 'VEB', 'STD', 'STE',
+                                                  'm-AVG_Acc', 'W-AVG_Acc']]
 
 # TODO: Perform statistical tests to check if there is a significant difference in the mean F1 scores
 #  among the three architectures -> not yet proved and working! (Feb 2024)
@@ -224,72 +185,35 @@ for model_name, model_path in model_paths.items():
 # else:
 #     print(f"No significant difference in means (F1 scores) among the architectures. The p-value is {p_value}")
 
+
 # Visualize the macro averages across all models
-create_and_save_boxplot(df=df_results_macro_averages,
-                        model_names=model_names,
-                        metric_name='F1',
-                        metric_cols=[0, 1, 2],
-                        title='Macro F1 Score',
-                        x_label='Architecture',
-                        y_label='F1 Score',
-                        filename='Macro_F1_Scores.pdf')
-create_and_save_boxplot(df=df_results_macro_averages,
-                        model_names=model_names,
-                        metric_name='AUC',
-                        metric_cols=[3, 4, 5],
-                        title='Macro AUC Score',
-                        x_label='Architecture',
-                        y_label='AUC Score',
-                        filename='Macro_AUC_Scores.pdf')
-create_and_save_boxplot(df=df_results_macro_averages,
-                        model_names=model_names,
-                        metric_name='Acc',
-                        metric_cols=[6, 7, 8],
-                        title='Macro Accuracy',
-                        x_label='Architecture',
-                        y_label='Accuracy',
-                        filename='Macro_Acc_Scores.pdf')
 # Boxplot for F1
-x_labels = [w.replace('_F1', '').replace('Final_Model', 'MACRO').replace('Multibranch', 'Multibranch \n MACRO')
-            for w in df_results_macro_averages.columns[0:3].values.tolist()]
-ax4 = df_results_macro_averages[[f'{model_names[0]}_F1',
-                                 f'{model_names[1]}_F1',
-                                 f'{model_names[2]}_F1']].boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
-ax4.set_title(f'Macro F1 Score')
-ax4.set_xlabel('Architecture')
-ax4.set_ylabel('F1 Score')
-ax4.set_xticklabels(x_labels, rotation=30)
-plt.tight_layout()
-plt.savefig(os.path.join('figures', 'Macro F1 Scores.pdf'), format="pdf", bbox_inches="tight")
-plt.show()
-
+create_and_save_boxplot_for_macro_avgs(df=df_results_macro_averages,
+                                       model_names=model_names,
+                                       metric_name='F1',
+                                       metric_cols=[0, 1, 2],
+                                       title='Macro F1 Score',
+                                       x_label='Architecture',
+                                       y_label='F1 Score',
+                                       filename='Macro_F1_Scores.pdf')
 # Boxplot for AUC
-x_labels = [w.replace('_AUC', '').replace('Final_Model', 'MACRO').replace('Multibranch', 'Multibranch \n MACRO')
-            for w in df_results_macro_averages.columns[3:6].values.tolist()]
-ax5 = df_results_macro_averages[[f'{model_names[0]}_AUC',
-                                 f'{model_names[1]}_AUC',
-                                 f'{model_names[2]}_AUC']].boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
-ax5.set_title(f'Macro AUC Score')
-ax5.set_xlabel('Architecture')
-ax5.set_ylabel('AUC Score')
-ax5.set_xticklabels(x_labels, rotation=30)
-plt.tight_layout()
-plt.savefig(os.path.join('figures', 'Macro AUC Scores.pdf'), format="pdf", bbox_inches="tight")
-plt.show()
-
+create_and_save_boxplot_for_macro_avgs(df=df_results_macro_averages,
+                                       model_names=model_names,
+                                       metric_name='AUC',
+                                       metric_cols=[3, 4, 5],
+                                       title='Macro AUC Score',
+                                       x_label='Architecture',
+                                       y_label='AUC Score',
+                                       filename='Macro_AUC_Scores.pdf')
 # Boxplot for Acc
-x_labels = [w.replace('_Acc', '').replace('Final_Model', 'MACRO').replace('Multibranch', 'Multibranch \n MACRO')
-            for w in df_results_macro_averages.columns[6:9].values.tolist()]
-ax6 = df_results_macro_averages[[f'{model_names[0]}_Acc',
-                                 f'{model_names[1]}_Acc',
-                                 f'{model_names[2]}_Acc']].boxplot(meanline=True, showmeans=True)  # , figsize=(15,8)
-ax6.set_title(f'Macro Accuracy')
-ax6.set_xlabel('Architecture')
-ax6.set_ylabel('Accuracy')
-ax6.set_xticklabels(x_labels, rotation=30)
-plt.tight_layout()
-plt.savefig(os.path.join('figures', 'Macro Acc Scores.pdf'), format="pdf", bbox_inches="tight")
-plt.show()
+create_and_save_boxplot_for_macro_avgs(df=df_results_macro_averages,
+                                       model_names=model_names,
+                                       metric_name='Acc',
+                                       metric_cols=[6, 7, 8],
+                                       title='Macro Accuracy',
+                                       x_label='Architecture',
+                                       y_label='Accuracy',
+                                       filename='Macro_Acc_Scores.pdf')
 
 # Write result to latex
 with open(os.path.join(base_path, 'paper_cross_valid_runs_summary.tex'), 'w') as file:
