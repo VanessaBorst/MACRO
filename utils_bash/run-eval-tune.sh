65abf4ff4ee615c6714d6415e7575c26c21bb703
vanessa <vanessa.borst@uni-wuerzburg.de>
2024-04-04 17:00:30 +0200

    Fixed Bash Utils

--- utils_bash/run-eval-tune.sh
+++ utils_bash/run-eval-tune.sh
@@ -2,11 +2,11 @@
 
 source venv/bin/activate
 
-REL_PATH="savedVM/models/Multibranch_MACRO_ParamStudy/0130_135327_ml_bs64_noConvRedBlock"
+REL_PATH="../savedVM/models/BaselineWithMultiHeadAttention_ParamStudy/0404_162144_ml_bs64"
 
 for dir in $(find $REL_PATH -mindepth 1 -maxdepth 1 -type d )
 do
     echo "Evaluating $dir ..."
-    python test.py --resume "$dir/model_best.pth" --test_dir "data/CinC_CPSC/test/preprocessed/250Hz/60s" --tune
+    python "../test.py" --resume "$dir/model_best.pth" --test_dir "../data/CinC_CPSC/test/preprocessed/250Hz/60s" --tune
 done
 
