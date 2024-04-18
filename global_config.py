c09942721f7e1699ad07a61706418005848fd290
vanessa <vanessa.borst@uni-wuerzburg.de>
2024-04-18 14:16:25 +0200

    Added method to suppress regex-matched warnings for entmax-conflict

--- global_config.py
+++ global_config.py
@@ -5,6 +5,7 @@ TUNE_TEMP_DIR = "/home/vab30xh/"
 
 ####### Configuration of Warning Messages #######
 import sys
+import re
 def suppress_warnings():
     """
     Suppress specific warnings
@@ -13,4 +14,14 @@ def suppress_warnings():
         import warnings
         warnings.simplefilter(action='ignore', category=FutureWarning)
         warnings.simplefilter(action='ignore', category=DeprecationWarning)
+
+        # Add a custom filter to suppress warnings starting with a specific message
+
+        # The following warning is caused since 'use_deterministic_algorithms' conflicts with the entmax
+        # activation function and hence, warn_only had to be set to True in 'use_deterministic_algorithms':
+        warnings.filterwarnings(action="ignore",
+                                category=UserWarning,
+                                message=re.escape("cumsum_cuda_kernel does not have a deterministic implementation, but"
+                                                  " you set 'torch.use_deterministic_algorithms(True, warn_only=True)'")
+                                )
 
